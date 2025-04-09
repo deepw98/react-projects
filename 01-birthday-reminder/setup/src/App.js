c512ae831707e13019a0b0e9020dc78eb09d9acc
deepw98 <143339991+deepw98@users.noreply.github.com>
2025-04-09 23:33:07 +0530

    birthday-remainder-practice

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,29 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [users,setUsers] = useState(data)
+  return (
+    <>
+    <div className='container'>
+      <h1>{users.length}Birthdays Today</h1>
+      <ul>
+        {users.map((user)=>{
+          const {id,image,name,age} = user
+          return(
+            <article key={id} className='person'>
+              <img src={image} alt={name} />
+              <div>
+                <h4>{name}</h4>
+                <p>{age} years</p>
+              </div>
+            </article>
+          )
+        })}
+      </ul>
+    </div>
+      
+    </>
+  )
 }
 
 export default App;
