73cded70fc095e7a5a6474dad3a85882f93b4b2b
deepw98 <143339991+deepw98@users.noreply.github.com>
2025-04-11 00:16:18 +0530

    work on slider start

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -2,8 +2,41 @@ import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
+
 function App() {
-  return <h2>slider project setup</h2>;
+  const [index,setIndex] = useState(0)
+
+  function prevBtn(){
+    if(index===0){
+      setIndex(data.length-1)
+    }else{
+      setIndex(index-1)
+    }
+  }
+
+  function nextBtn(){
+    
+  }
+  return (
+    <>
+      {data.map((dataItem)=>{
+        const {id,image,name,title,quote} = dataItem
+        return(
+          <article>
+            <h1>Reviews</h1>
+            <div>
+              <img src={image} alt={name} />
+              <h3>{name}</h3>
+              <h4>{title}</h4>
+              <p>{quote}</p>
+            </div>
+            <button ><FiChevronLeft/></button>
+            <button><FiChevronRight/></button>
+          </article>
+        )
+      })}
+    </>
+  )
 }
 
 export default App;
