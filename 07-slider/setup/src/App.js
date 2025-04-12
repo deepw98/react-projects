fa7290be4ea0965fdca3ca05eff6a0b0803ad83b
deepw98 <143339991+deepw98@users.noreply.github.com>
2025-04-12 21:56:11 +0530

    slider complete

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -5,6 +5,8 @@ import data from './data';
 
 function App() {
   const [index,setIndex] = useState(0)
+  const person = data[index]
+  const {id,name,title,image,quote} = person
 
   function prevBtn(){
     if(index===0){
@@ -15,26 +17,31 @@ function App() {
   }
 
   function nextBtn(){
-    
+    if(index===data.length-1){
+      setIndex(0)
+    }else{
+      setIndex(index+1)
+    }
   }
+
+  useEffect(()=>{
+    setTimeout(nextBtn,4000)
+  })
   return (
     <>
-      {data.map((dataItem)=>{
-        const {id,image,name,title,quote} = dataItem
-        return(
-          <article>
+         <article key={id}>
             <h1>Reviews</h1>
             <div>
-              <img src={image} alt={name} />
+              <img className='person-img' src={image} alt={name} />
               <h3>{name}</h3>
               <h4>{title}</h4>
               <p>{quote}</p>
             </div>
-            <button ><FiChevronLeft/></button>
-            <button><FiChevronRight/></button>
+            <button onClick={prevBtn}><FiChevronLeft/></button>
+            <button onClick={nextBtn}><FiChevronRight/></button>
+            <button><FaQuoteRight/></button>
           </article>
-        )
-      })}
+    
     </>
   )
 }
