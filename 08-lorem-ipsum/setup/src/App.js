f5e042f3c324188caa3c793894ced5dab1bf1164
deepw98 <143339991+deepw98@users.noreply.github.com>
2025-04-12 22:56:50 +0530

    lorem-ipsum half done

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,10 +1,36 @@
-import React, { useState } from 'react';
+import React, { use, useState } from 'react';
 import data from './data';
 function App() {
-  
-  
+  const [paranum,setParanum] = useState(0)
+  const [paragraphs,setParagraphs] = useState([])
+
+  function ShowParagraph(){
+    let input = document.querySelector('input')
+    setParagraphs([])
+    if(isNaN(input.value) || input.value<1  || input.value>9){
+      alert('Please enter value between 1 and 9')
+    }else{
+      setParanum(parseInt(input.value))
+      let newParagraphs = []
+      for(let i=0;i<=paranum;i++){
+        newParagraphs.push(<p key={i}>{data[i]}</p>)
+      }
+      setParagraphs(newParagraphs)
+    }
+   
+    
+  }
   return (
-  <h2>lorem ipsum project setup</h2>
+  <>
+    <div>
+      <h1>Tired of boring lorem ipsum?</h1>
+      <h2>paragraphs:</h2>
+      <input type="number" min={1} max={9}/>
+      <button onClick={ShowParagraph}>Generate</button>
+      {paragraphs}
+         
+    </div>
+  </>
     )
 }
 
